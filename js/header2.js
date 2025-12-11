98d3cadb468eadd771baf1175f39757608a49e1f
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-12-11 14:08:31 -0300

    erro

--- js/header2.js
+++ js/header2.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#page_body').insertAdjacentHTML('beforebegin', `
         <header>
-            <h1 class="menu-nome nome"><a href="./index.html">Miguel Oliveira</a></h1>
+            <h1 class="menu-nome nome"><a href="../index.html">Miguel Oliveira</a></h1>
             <ul id="menu">
                 <li class="menus" id="home"><a href="../index.html">Home</a></li>
                 <li class="menus" id="servicos"><a href="./servicos.html">Serviços</a></li>
