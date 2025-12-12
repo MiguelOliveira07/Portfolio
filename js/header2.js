6d6c1a9742e1d93e55ddaf33139cecd275d4a800
Miguel <miguelalberto6110@gmail.com>
2025-12-11 21:48:03 -0300

    update

--- js/header2.js
+++ js/header2.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
         <header>
             <h1 class="menu-nome nome audiowide-regular"><a href="../index.html">Miguel Oliveira</a></h1>
             <ul id="menu">
-                <li class="menus" id="home"><a href="../index.html">Home</a></li>
-                <li class="menus" id="servicos"><a href="./servicos.html">Serviços</a></li>
-                <li class="menus" id="educacao"><a href="./educacao.html">Formação</a></li>
+            <li class="menus audiowide-regular" id="home"><a href="../index.html">Home</a></li>
+            <li class="menus audiowide-regular" id="servicos"><a href="./servicos.html">Serviços</a></li>
+            <li class="menus audiowide-regular" id="educacao"><a href="./educacao.html">Formação</a></li>
             </ul>
         </header> 
     `)
